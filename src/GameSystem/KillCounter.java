c3e25e1997e61f672fb84933063a32abe0a596c2
beriloztr <oztr.beril@gmail.com>
2025-12-16 21:48:03 +0300

    added hashset class: KillCount

---
+++ src/GameSystem/KillCounter.java
@@ -0,0 +1,37 @@
+package GameSystem;
+import java.util.HashMap;
+
+public class KillCounter {
+
+	//Declare
+	private HashMap<String, Integer> counts;
+	//Constructor
+	public KillCounter() {
+        counts = new HashMap<>();
+        counts.put("TypeA", 0);
+        counts.put("TypeB", 0);
+        counts.put("TypeC", 0);
+    }
+
+    public void addKill(String enemyType) {
+        //Get current count, default 0 just in case
+        int current = counts.getOrDefault(enemyType, 0);
+        
+        //increment kill
+        counts.put(enemyType, current + 1);
+        System.out.println("Kill for: " + enemyType + " | Total kill: " + (current + 1));
+    }
+
+    public int getSpecificTypeKillCount(String enemyType) {
+        return counts.getOrDefault(enemyType, 0);
+    }
+    
+    public void printAllStats() { //for debug purpose
+        for (String key : counts.keySet()) {
+            System.out.println(key + ": " + counts.get(key));
+            //TypeA: 10 
+        }
+    }
+}
+	
+
